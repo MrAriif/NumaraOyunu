7f5199a0746402ace25e3abfa481ac4a2db095ea
Arif <Arif@DESKTOP-F9QS8PI>
2023-05-11 17:11:20 +0300

    Add project files.

---
+++ NumaraOyunu/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumaraOyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                NumberGame();
+
+            }
+            catch (Exception)
+            {
+
+                NumberGame();
+            }
+
+
+            Console.ReadLine();
+        }
+        static void NumberGame()
+        {
+            // Random metotunu 1 kere kullanabiliriz.
+            // Random rnd = new Random();
+            // int number = rnd.Next(1,101);
+            int number = new Random().Next(1, 101);  // Bu şekilde kullanılabilir.Tek satırda işlemi tamamlamış olduk.
+
+            int kalanHak = 10;
+
+            Console.Write("Tahmininizi Giriniz: ");
+            int userNumber = int.Parse(Console.ReadLine());
+
+            while (userNumber != number)
+            {
+                if (Math.Abs(number - userNumber) < 5)
+                {
+                    Console.WriteLine("SICAK");
+                }
+                else
+                {
+                    Console.WriteLine("SOĞUK");
+                }
+
+                userNumber = int.Parse(Console.ReadLine());
+
+                kalanHak--;
+
+                if (kalanHak == 0)
+                {
+                    Console.WriteLine("Hakkınız Bitti. Rakam :" + number);
+                    break;
+                }
+            }
+
+            if (userNumber == number)
+            {
+                Console.WriteLine("TEBRİKLER. KAZANDINIZ.");
+            }
+
+        }
+    }
+}
